e47c4ae996ef317b9452237f76f2711a366c177a
Martin Mašek <masek.ma@gmail.com>
2016-03-11 10:43:03 +0100

    logging basic

---
+++ Nuntius/Nuntius/Logging/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuntius.Logging
+{
+    class FileLogger : ILogger,IDisposable
+    {
+        private readonly string _filePath;
+        private StreamWriter writer;
+
+        public FileLogger(string filePath)
+        {
+            _filePath = filePath;
+            if (!File.Exists(filePath)) File.Create(filePath);
+            writer = new StreamWriter(_filePath);
+        }
+
+        public void Log(NuntiusMessage message)
+        {
+            writer.WriteLine($"Message: {message} {Environment.NewLine}");
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            writer.Dispose();
+        }
+    }
+}
---
+++ Nuntius/Nuntius/Logging/LoggingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuntius.Logging
+{
+    /// <summary>
+    /// This class provides extensions used for logging.
+    /// </summary>
+    public static class LoggingExtensions
+    {
+        public static IEventPropagator Log(this IEventPropagator source, ILogger logger)
+        {
+            // TODO: add logging
+            return source;
+        }
+
+        public static IEventPropagator LogToFile(this IEventPropagator source, string filePath)
+        {
+            var logger = new FileLogger(filePath);
+            source.Send += message =>
+            {
+                logger.Log(message);
+                return Task.FromResult<object>(null);
+            };
+            source.End += () => logger.Dispose();
+            return source;
+        }
+    }
+}
